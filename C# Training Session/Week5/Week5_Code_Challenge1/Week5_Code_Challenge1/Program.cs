2d1b23f6a5ed82d835c60c7f78fdad2c6be3a073
Unity-SeungwooLee <seungwoo.lee@unity3d.com>
2023-04-10 19:31:45 +0900

    Week5_Challenge1_done_Challenge2_WIP

---
+++ C# Training Session/Week5/Week5_Code_Challenge1/Week5_Code_Challenge1/Program.cs
@@ -0,0 +1,78 @@
+﻿/*Create an array replicator
+
+Create an array with the length of 5
+Use a for loop to ask user for 5 values to store in the array.
+Make a second array of 5.
+Use a Loop to iterate through the first array and copy the values into the second array.
+Use a another loop to write the values of both arrays to the console, both arrays should have the same values.
+*/
+
+
+int[] myNumbers = new int[5];
+int[] myNumbers2 = new int[5];
+
+for (int i = 0; i < myNumbers.Length; i++)
+{
+    myNumbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Numbers:");
+
+for (int i = 0; i < myNumbers.Length; i++)
+{
+    Console.Write(myNumbers[i] + " ");
+}
+
+
+for (int i = 0; i < myNumbers.Length; i++)
+{
+    myNumbers2[i] = myNumbers[i];
+}
+
+Console.WriteLine("\n------------------------------");
+
+for (int i = 0; i < myNumbers2.Length; i++)
+{
+    Console.Write(myNumbers2[i] + " ");
+}
+
+
+
+/* Chat GPT Answered
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+
+        // ask user for input and store in first array
+        for (int i = 0; i < array1.Length; i++)
+        {
+            Console.Write("Enter a number: ");
+            array1[i] = int.Parse(Console.ReadLine());
+        }
+
+        // copy values from first array to second array
+        for (int i = 0; i < array1.Length; i++)
+        {
+            array2[i] = array1[i];
+        }
+
+        // print values of both arrays horizontally
+        Console.Write("Array 1: ");
+        for (int i = 0; i < array1.Length; i++)
+        {
+            Console.Write(array1[i] + " ");
+        }
+
+        Console.Write("\nArray 2: ");
+        for (int i = 0; i < array2.Length; i++)
+        {
+            Console.Write(array2[i] + " ");
+        }
+    }
+}
+*/
---
+++ C# Training Session/Week5/Week5_Code_Challenge2/Week5_Code_Challenge2/Program.cs
@@ -0,0 +1,60 @@
+﻿/* Week5 Code Challenge2
+Create a name storage system using Lists and Loops.
+
+Program asks user what name they want to store.
+User enters the name and it's stored in a variable.
+Add that variable to a list of names.
+Loop this until the user enters "quit"
+Once this happens, use a loop to iterate through all the elements of the List and write each name to the console.
+*/
+
+Console.WriteLine("Please write names what you want to store.");
+
+
+List<string> names = new List<string>();
+
+string a;
+a = Console.ReadLine();
+
+if (a != "quit")
+{
+    names.Add(a);
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+/*Chat GPT Answered
+using System;
+using System.Collections.Generic;
+
+class MainClass {
+  public static void Main (string[] args) {
+    List<string> names = new List<string>();
+    string input;
+
+    do 
+    {
+      Console.Write("Enter a name (or 'quit' to exit): ");
+      input = Console.ReadLine();
+
+      if (input != "quit") 
+      {
+        names.Add(input);
+      }
+    } 
+    
+    while (input != "quit");
+
+    Console.WriteLine("\nAll names entered:");
+
+    foreach (string name in names) 
+    {
+      Console.WriteLine(name);
+    }
+  }
+}
+*/
