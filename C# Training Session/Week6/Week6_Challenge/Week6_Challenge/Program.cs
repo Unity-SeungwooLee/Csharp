54844501f77bbde7ec272316990fb3098143fa23
Unity-SeungwooLee <seungwoo.lee@unity3d.com>
2023-05-02 17:18:13 +0900

    Finished_Week6_Challenge

---
+++ C# Training Session/Week6/Week6_Challenge/Week6_Challenge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please write down dog1 name");
+        string a = Console.ReadLine();
+
+        Console.WriteLine("Please write down dog1 age");
+        int b;
+        while (!int.TryParse(Console.ReadLine(), out b))
+        {
+            Console.WriteLine("Invalid input. Please enter a Number.");
+        }
+
+        Console.WriteLine("Please write down dog2 name");
+        string c = Console.ReadLine();
+
+        Console.WriteLine("Please write down dog2 age");
+        int d;
+        while (!int.TryParse(Console.ReadLine(), out d))
+        {
+            Console.WriteLine("Invalid input. Please enter a Number.");
+        }
+
+        Dog dog1 = new Dog(a, b);
+        Dog dog2 = new Dog(c, d);
+    }
+}
+
+public class Dog
+{
+    public string Dogname;
+    public int Dogage;
+
+    public Dog(string name, int age)
+    {
+        Dogname = name;
+        Dogage = age;
+        Bark();
+        Sit();
+        Speak();
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine($"{Dogname} is barking \"Woof Woof\"");
+        Console.ReadKey();
+    }
+
+    public void Sit()
+    {
+        Console.WriteLine($"{Dogname} sat in front of you");
+        Console.ReadKey();
+    }
+
+    public void Speak()
+    {
+        Console.WriteLine($"I am {Dogage} years old");
+        Console.ReadKey();
+    }
+}
--- C# Training Session/Week6/Week6_Classes/Week6_Classes/Program.cs
+++ C# Training Session/Week6/Week6_Classes/Week6_Classes/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mercedes", "V8");
+            Car car2 = new Car("Audi", "V6");
+            
+            Car car3 = new Car();
+            car3.CarName = "Honda";
+            car3.InstallEngine("V8");
+            car3.Vroom();
+
+            Car car4 = new Car("Toyota", "V6", 4);
+
+            /*car1.Vroom();
+            Random rng =new Random();
+            rng = new Random();
+            */
+
+            //car1.CarName = "Mercedes";
+            //car2.CarName = "Audi";
+
+            //car1.Vroom();
+            //car1.IntallEngine("V8");
+
+            //car2.Vroom();
+            //car2.IntallEngine("V6");
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+        }
+
+    }
+
+    public class Car
+    {
+        public string CarName;
+        public int doorCount;
+        public string engineType;
+
+        public Car()
+        {
+            Console.WriteLine("No parameters were given, this is blank car");
+        }
+        public Car(string name, string _engineType)
+        {
+            CarName = name;
+            InstallEngine(_engineType);
+            Vroom();
+        }
+
+        public Car(string name, string _engineType, int doorCount)
+        {
+            Console.WriteLine("Executed 4 door code");
+            Console.ReadKey();
+        }
+
+        public void Vroom()
+        {
+            Console.WriteLine($"The {CarName} engines have started! VROOOOOOOM!");
+            Console.ReadKey();
+        }
+
+        public void InstallEngine(string _engineType)
+        {
+            engineType = _engineType;
+            Console.WriteLine(engineType + " Was installed in the " + CarName);
+            Console.ReadKey();
+        }
+    }
+
+    public class Player
+    {
+        public int id;
+        public Player() //This is a constructor
+        {
+            Random rnd = new Random();
+            id = rnd.Next();
+            Console.WriteLine(id);
+            Console.ReadKey();
+        }
+    }
+}
